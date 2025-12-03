51ce7eb9152c357c5e03ba8c92e6aad698992f58
wenyuan <xuwenyuan2016@163.com>
2025-12-03 11:09:12 +0800

    feat(framework.redis): add module

---
+++ ashore-framework/ashore-spring-boot-starter-redis/src/main/java/com/example/ashore/framework/redis/config/AshoreCacheProperties.java
@@ -0,0 +1,25 @@
+package com.example.ashore.framework.redis.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * Cache 配置项
+ */
+@ConfigurationProperties("ashore.cache")
+@Data
+@Validated
+public class AshoreCacheProperties {
+
+    /**
+     * {@link #redisScanBatchSize} 默认值
+     */
+    private static final Integer REDIS_SCAN_BATCH_SIZE_DEFAULT = 30;
+
+    /**
+     * redis scan 一次返回数量
+     */
+    private Integer redisScanBatchSize = REDIS_SCAN_BATCH_SIZE_DEFAULT;
+
+}
